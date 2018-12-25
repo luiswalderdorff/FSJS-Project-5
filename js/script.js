9f1099001a65b7f5061216d8bbd5c3d8571ebd70
Luis Waderdorff <walderdorff@live.de>
2018-12-25 12:45:31 +0100

    Modal windows dom traversal probleme

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Did they automaticially capitalize letters with CSS?
 // Variables
 
 const $gallery = $("#gallery");
@@ -19,6 +20,16 @@ fetch("https://randomuser.me/api/?results=12&nat=gb")
   .then(function(data) {
     const users = data.results;
     displayUsers(users);
+    $(".card").click(function(event) {
+      const click = event.target;
+      console.log(click);
+      // Muss noch richtige Parentnodes und so hinzufügen, damit es immer richtig feuert
+      // for(let i = 0; i < $(".modal-container").length; i++) {
+      //   if(click.parentNode.children[0].children[0].src === $(".modal-container")[i].children[0].children[1].children[0].src || click.parentNode.parentNode.children[0].children[0].src === $(".modal-container")[i].children[0].children[1].children[0].src || click.parentNode.parentNode.parentNode.children[0].children[0].src === $(".modal-container")[i].children[0].children[1].children[0].src) {
+      //     $(".modal-container").eq(i).show();
+      //   }
+      // }
+    })
   })
 
 
@@ -40,30 +51,32 @@ fetch("https://randomuser.me/api/?results=12&nat=gb")
     $gallery.append(userHTML);
 
     // Hidden Modal Interface: Try to create it when clicked. The one that is needed
-    // var modalHTML = '<div class="modal-container">';
-    // data.forEach(function(results) {
-    //     modalHTML += `    <div class="modal">
-    //                         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-    //                         <div class="modal-info-container">
-    //                             <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-    //                             <h3 id="name" class="modal-name cap">name</h3>
-    //                             <p class="modal-text">email</p>
-    //                             <p class="modal-text cap">city</p>
-    //                             <hr>
-    //                             <p class="modal-text">(555) 555-5555</p>
-    //                             <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-    //                             <p class="modal-text">Birthday: 10/21/2015</p>
-    //                         </div>
-    //                       </div>
-    //
-    //                     // IMPORTANT: Below is only for exceeds tasks
-    //                     <div class="modal-btn-container">
-    //                         <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-    //                         <button type="button" id="modal-next" class="modal-next btn">Next</button>
-    //                     </div>`
-    // }) // end each
-    // modalHTML += "</div>"
-    // $("body").append(modalHTML); //noch anpassen
+    var modalHTML = '';
+    data.forEach(function(results) {
+        modalHTML += `    <div class="modal-container">
+                            <div class="modal">
+                              <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                              <div class="modal-info-container">
+                                  <img class="modal-img" src="${results.picture.large}" alt="profile picture">
+                                  <h3 id="name" class="modal-name cap">${results.name.first} ${results.name.last}</h3>
+                                  <p class="modal-text">>${results.email}</p>
+                                  <p class="modal-text cap">${results.location.city}</p>
+                                  <hr>
+                                  <p class="modal-text">${results.phone}</p>
+                                  <p class="modal-text">${results.location.street}, ${results.location.city}, ${results.location.postcode}</p>
+                                  <p class="modal-text">${results.dob.date}</p>
+                              </div>
+                            </div>
+
+                          <div class="modal-btn-container">
+                              <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                              <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                          </div>
+                        </div>`
+    }) // end each
+    modalHTML += ""
+    $("body").append(modalHTML); //noch anpassen
+    $(".modal-container").hide();
 
 
     }; // end each
